a01af730eaa3afb802ab62f9661f2b265e2d8688
mohammedelghazaly67@gmail.com <warendragon67@gmail.com>
2026-02-16 23:24:24 +0200

    Finished app layout section

---
+++ lib/features/app_layout/data/model/app_layout_data.dart
@@ -0,0 +1,25 @@
+import 'package:elevate_tracking_app/core/theme/app_icons.dart';
+
+class AppLayoutData {
+  final String imagePath;
+  final int index;
+  final String title;
+
+  AppLayoutData({
+    required this.imagePath,
+    required this.index,
+    required this.title,
+  });
+
+  static List<AppLayoutData> getAppLayoutData() {
+    return [
+      AppLayoutData(imagePath: AppIcons.iconsHome, index: 0, title: "Title 1"),
+      AppLayoutData(imagePath: AppIcons.orders, index: 1, title: "Title 2"),
+      AppLayoutData(
+        imagePath: AppIcons.iconsProfile,
+        index: 2,
+        title: "Title 3",
+      ),
+    ];
+  }
+}
