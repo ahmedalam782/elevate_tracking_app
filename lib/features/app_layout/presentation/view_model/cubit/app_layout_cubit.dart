6fee8dba1b6378ab0cb81d3097806f7acfb2eb75
mayaryasser2005 <mayaryasser300@gmail.com>
2026-03-05 00:33:09 +0200

    finish feature success page design

--- lib/features/app_layout/presentation/view_model/cubit/app_layout_cubit.dart
+++ lib/features/app_layout/presentation/view_model/cubit/app_layout_cubit.dart
@@ -1,13 +1,7 @@
-import 'package:elevate_tracking_app/core/config/di/injectable_config.dart';
 import 'package:elevate_tracking_app/features/app_layout/presentation/view_model/cubit/app_layout_state.dart';
 import 'package:elevate_tracking_app/features/orders_tap/presentation/view/pages/orders_page.dart';
-import 'package:elevate_tracking_app/features/home/domain/use_cases/accept_order_use_case.dart';
-import 'package:elevate_tracking_app/features/home/domain/use_cases/get_pending_orders_use_case.dart';
 import 'package:elevate_tracking_app/features/home/presentation/view/pages/home_page.dart';
-import 'package:elevate_tracking_app/features/home/presentation/view_model/cubit/home_cubit.dart';
-import 'package:elevate_tracking_app/features/home/presentation/view_model/cubit/home_events.dart';
 import 'package:elevate_tracking_app/features/profile/presentation/view/pages/profile_page.dart';
-import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
