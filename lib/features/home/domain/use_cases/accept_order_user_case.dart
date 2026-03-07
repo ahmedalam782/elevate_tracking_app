197f60c85c4a1668f11622a4ca2d8e26a23f0a2f
Ahmed Mohamed Osman <ahmedalam4887482@gmail.com>
2026-03-07 15:12:12 +0200

    feat: implement accept order use case

---
+++ lib/features/home/domain/use_cases/accept_order_user_case.dart
@@ -0,0 +1,14 @@
+import 'package:elevate_tracking_app/core/config/base_response/result.dart';
+import 'package:elevate_tracking_app/features/home/data/models/accept_order_response/accept_order_response.dart';
+import 'package:elevate_tracking_app/features/home/domain/repositories/home_repository.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class AcceptOrderUserCase {
+  final HomeRepository homeRepository;
+
+  AcceptOrderUserCase({required this.homeRepository});
+  Future<Result<AcceptOrderResponse>> call(String id) {
+    return homeRepository.acceptOrder(id);
+  }
+}
